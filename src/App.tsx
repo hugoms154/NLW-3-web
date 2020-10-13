9cb948caf6a8bd1d1ff3d16e9c70476538669957
hugo miranda <hugo.miranda.souza@gmail.com>
2020-10-12 21:13:16 -0300

    config Eslint, Prettier, EditorConfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div>
-        <h1>title</h1>
+      <h1>title</h1>
     </div>
   );
-}
+};
 
 export default App;
