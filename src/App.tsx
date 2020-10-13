8a4af33b2431ce71184b1ef38f1f8ffff335861d
hugo miranda <hugo.miranda.souza@gmail.com>
2020-10-13 09:42:51 -0300

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
+import Routes from './routes';
+
 const App: React.FC = () => {
-  return (
-    <div>
-      <h1>title</h1>
-    </div>
-  );
+  return <Routes />;
 };
 
 export default App;
